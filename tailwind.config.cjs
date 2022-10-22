f2c6eeeb7a2437a6cfeb6ef86593d0d9ea9662db
cyrus01337 <cyrus21337@gmail.com>
2022-10-22 05:47:29 +0100

    Change PostCSS to JSON and add TailwindCSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const daisyUI = require("daisyui");
+const tailwindCSSTypography = require("@tailwindcss/typography");
+
+module.exports = {
+    content: ["src/client/**/*.jsx"],
+    daisyui: {
+        // https://daisyui.com/docs/themes/#-4
+        themes: ["light"],
+    },
+    plugins: [daisyUI, tailwindCSSTypography],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Inter", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+};
