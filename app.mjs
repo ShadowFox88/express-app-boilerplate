c487b10095f4b1dee62f0789b460de60dec550cc
cyrus01337 <cyrus21337@gmail.com>
2022-10-22 08:21:56 +0100

    Setup optional React SSR

--- app.mjs
+++ app.mjs
@@ -1,5 +1,8 @@
+import path from "path";
 import process from "process";
 
+// import { createEngine } from "express-react-views";
+import expressReactViews from "express-react-views";
 import express from "express";
 
 import routes from "./src/server/routes";
@@ -12,6 +15,10 @@ const app = express();
 
 console.log(`NODE_ENV is "${process.env.NODE_ENV}"`);
 app.enable("trust proxy");
+app.set("views", path.resolve("./src/server"));
+app.set("view engine", "jsx");
+
+app.engine("jsx", expressReactViews.createEngine());
 app.use(express.static("./dist"));
 app.use(express.json(), express.urlencoded({ extended: false }), routes);
 
